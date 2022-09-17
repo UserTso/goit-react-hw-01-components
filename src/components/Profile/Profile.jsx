4181f6904ea95cf930300d5e4df7522318790b35
Sergey Tkachenko <106906276+UserTso@users.noreply.github.com>
2022-09-18 01:07:23 +0300

    first task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+export default function UserProfile({username, tag, location, avatar, stats: {followers, views, likes}}) {
+  // const {followers, views, likes} = stats;
+    return (
+        <div className={css.profile}>
+<div className={css.description}>
+  <img
+    src={avatar}
+    alt={tag}
+    className={css.avatar}
+    width={100} 
+  />
+  <p className={css.name}><h1>{username}</h1></p>
+  <p className={css.tag}>{tag}</p>
+  <p className={css.location}>{location}</p>
+</div>
+<ul className={css.stats}>
+    <li className={css.item}>
+      <span className={css.label}>Followers</span>
+      <span className={css.quantity}><b>{followers}</b></span>
+    </li>
+    <li className={css.item}>
+      <span className={css.label}>Views</span>
+      <span className={css.quantity}><b>{views}</b></span>
+    </li>
+    <li className={css.item}>
+      <span className={css.label}>Likes</span>
+      <span className={css.quantity}><b>{likes}</b></span>
+    </li>
+  </ul>
+</div>
+);
+}
+
+UserProfile.prototypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  followers: PropTypes.number,
+  views: PropTypes.number,
+  likes: PropTypes.number,
+}
+
+
